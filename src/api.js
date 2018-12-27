3f77fa3cb83604f5fe16a970ca7004f2bb219dae
Ron Soeum <ronsoeum@gmail.com>
2018-12-26 18:23:10 -0600

    added real time search, and fixed bugs

--- src/api.js
+++ src/api.js
@@ -2,66 +2,139 @@
 
 const $ = require ("jQuery");
 
-const getMovies = () => {
+let title, genre, rating;
+
+// Get Request - Movie Data in JSON
+const getMoviesJson = () => {
   return fetch('/api/movies')
       .then(response => response.json());
 };
 
+// Error Handler
 const err = (error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 };
 
-// Featured Movies
-// const fMovies = movies => {
-//   $('#title-fMovie1').text(movies[0].title);
-//   $('#title-fMovie2').text(movies[1].title);
-//   $('#title-fMovie3').text(movies[2].title);
-//   $('#title-fMovie4').text(movies[3].title);
-//   $('#rating-fMovie1').text(movies[0].rating);
-//   $('#rating-fMovie2').text(movies[1].rating);
-//   $('#rating-fMovie3').text(movies[2].rating);
-//   $('#rating-fMovie4').text(movies[3].rating);
-// };
-
-// Movie Index
-const index  = (movies) => {
-  $('#loading').hide();
-  movies.forEach(({title, rating, id}) => {
-    $('#movies').append(
-        `<div class="card-body col-3"><h3>${title}</h3><p>${rating}</p></div>`
-    );
-  });
+// Get Request - Render Movie Index
+const renderIndexMovies  = (movies) => {
+  $('.loadingIndex').hide();
+  movies.filter( ({title, genre, rating, id}) => {
+    if(id < 33){
+      $('#movies').append(`<div class="card-body col-3">
+        <h3>${title}</h3><p>${genre}</p><p>${rating}</p></div>`);
+    }
+  })
 };
 
-// Post Request Using Promise
-let title = '';
-let rating = '';
+// Get Request - Render Wish List
+const renderWishMovies  = (movies) => {
+  $('.loadingWish').hide();
+  movies.filter( ({title, genre, rating, id}) => {
+    if(id > 32){
+      $('#wMovies').append(`<div id="${id}" class="wish-movie card-body col-3">
+        <h3 class="d-inline-block">${title}</h3><button class="delete-button float-right">X</button>
+        <p>${genre}</p><p>${rating}</p></div>`);
+    }
+  })
+};
 
+// Post Request - Add New Movie
 $('#addButton').click(() => {
+  $('.loadingWish').show();
   title = $('#title').val();
+  genre = $('#genre').val();
   rating = $('#rating').val();
-  console.log(title);
-
-
-
-  let newMovie = {title: title, rating: rating};
   const fetchOptions = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(newMovie)
+    body: JSON.stringify({title: title, genre: genre, rating: rating})
   };
-  const postMovie = () => fetch('/api/movies', fetchOptions);
-  postMovie();
-
+  fetch('/api/movies', fetchOptions);
+  $('#wMovies').empty();
+  getMoviesJson().then(renderWishMovies).catch(err);
 });
 
-module.exports = {
-  getMovies,
-  // fMovies,
-  index,
+// Delete Request - Delete Movie From Wish List
+const deleteMovies = () => {
+  $('#wMovies').on("click", ".delete-button", function () {
+    let id = ($('.wish-movie').attr("id"));
+    if(confirm("Do you want to delete this from the list?")){
+      fetch(`/api/movies/${id}`, {method: 'DELETE'});
+      $('.loadingWish').show();
+      $('#wMovies').empty();
+      getMoviesJson().then(renderWishMovies).catch(err);
+    }
+  });
+};
+
+// Real Time Search By Movie Title
+const movieIndexByTitle = () => {
+  $('#search-title').on('input', function(){
+    getMoviesJson().then(movies => {
+      $('#movies').empty();
+      movies.map(movie => {
+        $('#search-title').val().split(" ").map(function (word) {
+          let title = movie.title, genre = movie.genre, rating = movie.rating;
+          if(movie.id <33){
+            if(movie.title.toLowerCase().indexOf(word.toLowerCase()) != -1){
+              $('#movies').append(`<div class="card-body col-3"><h3>${title}</h3><p>${genre}</p><p>${rating}</p></div>`);
+            }
+          }
+        })
+      })
+    });
+  });
+};
+
+// Real Time Search By Movie Genre
+const movieIndexByGenre = () => {
+  $('#search-genre').on('input', function(){
+    getMoviesJson().then(movies => {
+      $('#movies').empty();
+      movies.map(movie => {
+        $('#search-genre').val().split(" ").map(function (word) {
+          let title = movie.title, genre = movie.genre, rating = movie.rating;
+          if(movie.id <33){
+            if(movie.genre.toLowerCase().indexOf(word.toLowerCase()) != -1){
+              $('#movies').append(`<div class="card-body col-3"><h3>${title}</h3><p>${genre}</p><p>${rating}</p></div>`);
+            }
+          }
+        })
+      })
+    });
+  });
+};
+
+// Real Time Search By Movie Rating
+const movieIndexByRating = () => {
+  $('#search-rating').on('input', function(){
+    getMoviesJson().then(movies => {
+      $('#movies').empty();
+      movies.map(movie => {
+        $('#search-rating').val().split(" ").map(function (word) {
+          let title = movie.title, genre = movie.genre, rating = movie.rating;
+          if(movie.id < 33){
+            if(movie.rating.indexOf(word) != -1){
+              $('#movies').append(`<div class="card-body col-3"><h3>${title}</h3><p>${genre}</p><p>${rating}</p></div>`);
+            }
+          }
+        })
+      })
+    });
+  });
+};
+
+export {
+  getMoviesJson,
+  renderIndexMovies,
+  renderWishMovies,
+  movieIndexByTitle,
+  movieIndexByGenre,
+  movieIndexByRating,
+  deleteMovies,
   err
 };
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 "use strict";
 
 const $ = require ("jQuery");
-const {getMovies, err, index} = require('./api.js');
+
+import {
+    getMoviesJson,
+    renderIndexMovies,
+    renderWishMovies,
+    movieIndexByTitle,
+    movieIndexByGenre,
+    movieIndexByRating,
+    deleteMovies,
+    err} from './api.js';
 
 $(document).ready(function() {
 
-    getMovies().then((movies) => {
-      console.log('Here are all the movies:');
-      movies.forEach(({title, rating, id}) => {
-        console.log(`id#${id} - ${title} - rating: ${rating}`);
-      });
-    }).catch(err);
+    // Render Index Movies
+    getMoviesJson().then(renderIndexMovies).catch(err);
 
-    // // Render Featured Movies
-    // getMovies().then(fMovies).catch(err);
+    // Reset Index Movies
+    $('#resetIndex').click(() => {
+        $('#movies').empty();
+        $('.loadingIndex').show();
+        getMoviesJson().then(renderIndexMovies).catch(err);
+    });
 
-    // Render Movie Index
-    getMovies().then(index).catch(err);
+    // Real Time Search By Movie Title
+    movieIndexByTitle();
+
+    // Real Time Search By Movie Genre
+    movieIndexByGenre();
+
+    // Real Time Search By Movie Rating
+    movieIndexByRating();
+
+    // Render Wish Movies
+    getMoviesJson().then(renderWishMovies).catch(err);
+
+    // Delete Wish Movies
+    deleteMovies();
 
 });// Ready
