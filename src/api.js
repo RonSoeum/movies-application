79f09464cfc14c8e810a51fd7f068cae54cd2891
Ron Soeum <ronsoeum@gmail.com>
2018-12-31 12:25:35 -0600

    improved code readability

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const $ = require ("jQuery");
-let title, genre, rating;
 
 // Get Request - Movie Data in JSON
 const getMoviesJson = () => {
@@ -11,7 +10,7 @@ const getMoviesJson = () => {
 
 // Error Handler
 const err = (error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
+  console.log('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 };
 
@@ -24,82 +23,10 @@ const renderIndexMovies  = (movies) => {
       for (; rating >= 1; rating--){
         stars += `<i class="far fa-star"></i>`;
       }
-      $('#movies').append(`<div class="card-body col-3"><div class=""><h2>${title}</h2><p>${genre}</p><h2>${stars}</h2></div></div>`);
+      $('#movies').append(`<div class="card-body col-3"><div class="">
+        <h2>${title}</h2><p>${genre}</p><h2>${stars}</h2></div></div>`);
     }
   })
-};
-
-// Real Time Search By Movie Title
-const movieIndexByTitle = () => {
-  $('#search-title').on('input', function(){
-    getMoviesJson().then(movies => {
-      $('#movies').empty();
-      movies.map(movie => {
-        $('#search-title').val().split(" ").map(function (word) {
-          let title = movie.title, genre = movie.genre, rating = movie.rating;
-          if(movie.id <33){
-            let stars = '';
-            for (; rating >= 1; rating--){
-              stars += `<i class="far fa-star"></i>`;
-            }
-            if(movie.title.toLowerCase().indexOf(word.toLowerCase()) != -1){
-              $('#movies').append(`<div class="card-body col-3"><h2>${title}</h2><p>${genre}</p><p>${stars}</p></div>`);
-            }
-          }
-        })
-      })
-    });
-  });
-};
-
-// Real Time Search By Movie Genre
-const movieIndexByGenre = () => {
-  $('#search-genre').on('input', function(){
-    getMoviesJson().then(movies => {
-      $('#movies').empty();
-      movies.map(movie => {
-        $('#search-genre').val().split(" ").map(function (word) {
-          let title = movie.title, genre = movie.genre, rating = movie.rating;
-          if(movie.id <33){
-            let stars = '';
-            for (; rating >= 1; rating--){
-              stars += `<i class="far fa-star"></i>`;
-            }
-            if(movie.genre.toLowerCase().indexOf(word.toLowerCase()) != -1){
-              $('#movies').append(`<div class="card-body col-3"><h2>${title}</h2><p>${genre}</p><p>${stars}</p></div>`);
-            }
-          }
-        })
-      })
-    });
-  });
-};
-
-// Real Time Search By Movie Rating
-const movieIndexByRating = () => {
-  $('#search-rating').on('input', function(){
-    getMoviesJson().then(movies => {
-      $('#movies').empty();
-      if($('#search-rating option:selected').val() === `--search by rating--`){
-        getMoviesJson().then(renderIndexMovies).catch(err);
-      }else{
-        movies.map(movie => {
-          $('#search-rating option:selected').val().split(" ").map(function(word){
-            let title = movie.title, genre = movie.genre, rating = movie.rating;
-            if(movie.id < 33){
-              let stars = '';
-              for(; rating >= 1; rating--){
-                stars += `<i class="far fa-star"></i>`;
-              }
-              if(movie.rating.indexOf(word) != -1){
-                $('#movies').append(`<div class="card-body col-3"><h2>${title}</h2><p>${genre}</p><p>${stars}</p></div>`);
-              }
-            }
-          });
-        });
-      }
-    });
-  });
 };
 
 // Get Request - Render Wish List
@@ -112,12 +39,13 @@ const renderWishMovies  = (movies) => {
         stars += `<i class="far fa-star"></i>`;
       }
       $('#wMovies').append(`<div class="card-body col-3 position-relative"><h2>${title}</h2>
-        <button class="delete-button position-absolute btn" data-id="${id}">X</button><p>${genre}</p><p>${stars}</p></div>`);
+        <button class="delete-button position-absolute btn" data-id="${id}">X</button>
+        <p>${genre}</p><p>${stars}</p></div>`);
     }
   })
 };
 
-// Render Select Option For Wish List
+// Render Select Option To Edit Wish List
 const renderWishSelect = () => {
   getMoviesJson().then(movies => {
     movies.map(({title, id}) => {
@@ -128,67 +56,97 @@ const renderWishSelect = () => {
   });
 };
 
-// Post Request - Add Movie To Wish List
-$('#add-button').click(() => {
-  $('.loadingWish').show();
-  title = $('#title').val();
-  genre = $('#genre').val();
-  rating = $('#rating').val();
-  fetch('/api/movies', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({title: title, genre: genre, rating: rating})
-  });
-  $('#wMovies').empty();
-  getMoviesJson().then(renderWishMovies).catch(err);
+// Reset Wish List Form
+const resetWishListForm = () => {
   $("#edit-movie > *:not('.not-remove')").remove();
   renderWishSelect();
-});
+  $("#userPreferred")[0].reset();
+};
 
-// Put Request - Edit Movie In Wish List
-$('#edit-button').click(() => {
-  $('.loadingWish').show();
-  title = $('#title').val();
-  genre = $('#genre').val();
-  rating = $('#rating').val();
-  let id = $('#edit-movie option:selected').val();
-  fetch(`/api/movies/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({title: title, genre: genre, rating: rating})
+// Real Time Search By Movie Title
+const movieIndexByTitle = () => {
+  $('#search-title').on('input', () => {
+    getMoviesJson().then(movies => {
+      $('#movies').empty();
+      movies.map(movie => {
+        $('#search-title').val().split(" ").map(word => {
+          let title = movie.title, genre = movie.genre, rating = movie.rating;
+          if(movie.id <33){
+            let stars = '';
+            for (; rating >= 1; rating--){
+              stars += `<i class="far fa-star"></i>`;
+            }
+            if(movie.title.toLowerCase().indexOf(word.toLowerCase()) != -1){
+              $('#movies').append(`<div class="card-body col-3">
+                <h2>${title}</h2><p>${genre}</p><p>${stars}</p></div>`);
+            }
+          }
+        })
+      })
+    });
   });
-  $('#wMovies').empty();
-  getMoviesJson().then(renderWishMovies).catch(err);
-  $("#edit-movie > *:not('.not-remove')").remove();
-  renderWishSelect();
-});
+};
 
+// Real Time Search By Movie Genre
+const movieIndexByGenre = () => {
+  $('#search-genre').on('input', () => {
+    getMoviesJson().then(movies => {
+      $('#movies').empty();
+      movies.map(movie => {
+        $('#search-genre').val().split(" ").map(word => {
+          let title = movie.title, genre = movie.genre, rating = movie.rating;
+          if(movie.id <33){
+            let stars = '';
+            for (; rating >= 1; rating--){
+              stars += `<i class="far fa-star"></i>`;
+            }
+            if(movie.genre.toLowerCase().indexOf(word.toLowerCase()) != -1){
+              $('#movies').append(`<div class="card-body col-3"><h2>${title}</h2>
+                <p>${genre}</p><p>${stars}</p></div>`);
+            }
+          }
+        })
+      })
+    });
+  });
+};
 
-// Delete Request - Delete Movie From Wish List
-$('#wMovies').on('click', ".delete-button", (e) => {
-  let id = $(e.target).data('id');
-  if(confirm("Do you want to delete this from the list?")){
-    fetch(`/api/movies/${id}`, {method: 'DELETE'});
-    $('.loadingWish').show();
-    $('#wMovies').empty();
-    getMoviesJson().then(renderWishMovies).catch(err);
-    $("#edit-movie > *:not('.not-remove')").remove();
-    renderWishSelect();
-  }
-});
+// Real Time Search By Movie Rating
+const movieIndexByRating = () => {
+  $('#search-rating').on('input', () => {
+    getMoviesJson().then(movies => {
+      $('#movies').empty();
+      if($('#search-rating option:selected').val() === `--search by rating--`){
+        getMoviesJson().then(renderIndexMovies).catch(err);
+      }else{
+        movies.map(movie => {
+          $('#search-rating option:selected').val().split(" ").map(word => {
+            let title = movie.title, genre = movie.genre, rating = movie.rating;
+            if(movie.id < 33){
+              let stars = '';
+              for(; rating >= 1; rating--){
+                stars += `<i class="far fa-star"></i>`;
+              }
+              if(movie.rating.indexOf(word) != -1){
+                $('#movies').append(`<div class="card-body col-3"><h2>${title}</h2>
+                  <p>${genre}</p><p>${stars}</p></div>`);
+              }
+            }
+          });
+        });
+      }
+    });
+  });
+};
 
 export {
   getMoviesJson,
   renderIndexMovies,
   renderWishMovies,
   renderWishSelect,
+  resetWishListForm,
   movieIndexByTitle,
   movieIndexByGenre,
   movieIndexByRating,
   err
 };
-
--- src/index.js
+++ src/index.js
@@ -7,21 +7,92 @@ import {
     renderIndexMovies,
     renderWishMovies,
     renderWishSelect,
+    resetWishListForm,
     movieIndexByTitle,
     movieIndexByGenre,
     movieIndexByRating,
     err} from './api.js';
 
-$(document).ready(function() {
+$(document).ready(() => {
 
-    // Render Index Movies
+    // Render Movie Index
     getMoviesJson().then(renderIndexMovies).catch(err);
 
-    // Reset Index Movies
+    // Reset Movie Index With Button
     $('#resetIndex').click(() => {
+        $("#resetIndex").prop("disabled", true);
+        $("#search-form")[0].reset();
         $('#movies').empty();
         $('.loadingIndex').show();
-        getMoviesJson().then(renderIndexMovies).catch(err);
+        getMoviesJson().then(renderIndexMovies)
+            .then(() => $("#resetIndex").prop("disabled", false)).catch(err);
+    });
+
+    // Render Wish List
+    getMoviesJson().then(renderWishMovies).catch(err);
+
+    // Render Select Option To Edit Wish List
+    renderWishSelect();
+
+    // Post Request - Add Movie To Wish List
+    $('#add-button').click(() => {
+        $('#add-button').prop("disabled", true);
+        $('#wMovies').empty();
+        $('.loadingWish').show();
+        fetch('/api/movies', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                title: $('#title').val(),
+                genre: $('#genre').val(),
+                rating: $('#rating option:selected').val()
+            })
+        })
+            .then(() => {
+                getMoviesJson().then(renderWishMovies)
+                    .then(() => $('#add-button').prop("disabled", false)).catch(err);
+                resetWishListForm();
+            }).catch(err);
+    });
+
+    // Put Request - Edit Movie In Wish List
+    $('#edit-button').click(() => {
+        $('#edit-button').prop("disabled", true);
+        if($('#edit-movie option:selected').val() == 'default'){
+            $('#edit-button').prop("disabled", false);
+        }else{
+            $('#wMovies').empty();
+            $('.loadingWish').show();
+            let id = $('#edit-movie option:selected').val();
+            fetch(`/api/movies/${id}`, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    title: $('#title').val(),
+                    genre: $('#genre').val(),
+                    rating: $('#rating option:selected').val()
+                })
+            })
+                .then(() => {
+                    getMoviesJson().then(renderWishMovies)
+                        .then(() => $('#edit-button').prop("disabled", false)).catch(err);
+                    resetWishListForm();
+                }).catch(err);
+        }
+    });
+
+    // Delete Request - Delete Movie From Wish List
+    $('#wMovies').on('click', ".delete-button", (e) => {
+        $('#wMovies').empty();
+        $('.loadingWish').show();
+        let id = $(e.target).data('id');
+        if(confirm("Do you want to delete this from the list?")){
+            fetch(`/api/movies/${id}`, {method: 'DELETE'})
+                .then(() => {
+                    getMoviesJson().then(renderWishMovies).catch(err);
+                    resetWishListForm();
+                }).catch(err);
+        }
     });
 
     // Real Time Search By Movie Title
@@ -32,11 +103,5 @@ $(document).ready(function() {
 
     // Real Time Search By Movie Rating
     movieIndexByRating();
-
-    // Render Wish Movies
-    getMoviesJson().then(renderWishMovies).catch(err);
-
-    // Render Wish Select Options
-    renderWishSelect();
 
 });// Ready
